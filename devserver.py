6b16f32a156bf5046032cbbd0df789571e4ce0c6
jess406 <jess406@users.noreply.github.com>
2026-07-09 09:59:36 -0400

    Auto-save: 2026-07-09 09:59:36

--- devserver.py
+++ devserver.py
@@ -12,6 +12,7 @@ class NoCacheHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
+        self.send_header("Clear-Site-Data", '"cache"')
         super().end_headers()
 
 
